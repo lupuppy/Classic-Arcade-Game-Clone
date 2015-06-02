b42f2548b0a5eea0967ef04854345ef0e8cc2dfc
Luna Yao <xinglu@Xinglus-MacBook-Air.local>
2015-06-02 10:01:32 -0700

    added all the files

---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    //set initial location
+    this.x = 0;
+    this.y = Math.floor(Math.random()*3+1);
+    //set speed
+    this.speed = Math.floor(Math.random()*100+40);
+    //load image
+    this.sprite = 'images/enemy-bug.png';
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if(this.x<505){
+        this.x += dt * this.speed;
+    }else{
+        this.x = 0;
+    }
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y * 83);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+// starting position
+    this.x = Math.floor(Math.random()*4);
+    this.y = Math.floor(Math.random()*2+4);
+//load image
+    this.sprite = 'images/char-boy.png';
+
+}
+
+Player.prototype.update = function(){
+        
+    for (var i = 0; i<4; i++){
+        if((allEnemies[i].x<this.x*101) && (this.x*101<(allEnemies[i].x+101)) && (this.y === allEnemies[i].y)){
+            this.x = Math.floor(Math.random()*4);
+            this.y = Math.floor(Math.random()*2+4);
+            
+       }else if(this.y === 0){
+             this.x = Math.floor(Math.random()*4);
+             this.y = Math.floor(Math.random()*2+4);
+       }
+    }
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+}
+
+Player.prototype.handleInput = function(input){
+    if (input === 'left'){if(this.x>0){
+            this.x -= 1;
+        }
+    }else if (input === 'right'){if (this.x<4){
+            this.x += 1;
+        }
+    }else if (input === 'up'){if (this.y>0){
+            this.y -= 1;
+        }
+    }else if(input === 'down'){if (this.y<5){
+            this.y += 1;
+        }
+    }
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
+function createEnemiesArray(){
+    
+    for (var i = 0; i<4; i++){
+        allEnemies[i] = new Enemy();
+    }
+}
+
+// Place the player object in a variable called player
+var player;
+function createNewPlayer(){
+    player = new Player();
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
